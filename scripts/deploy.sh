6841fb6633089cc0e4d0ba52b27de36804f13d5c
crysthofferattier <crysthofferattier@gmail.com>
2022-10-07 20:57:35 +0100

    Production version 2022-10-07 20:57:35

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+APP_VERSION=$(cat index.html | grep app-title | cut -d'.' -f 2)
+APP_VERSION=$(($APP_VERSION + 1))
+
+echo "App Version"
+echo $APP_VERSION
+
 ng build --prod --output-path docs --base-href="https://crysthofferattier.github.io/" \
 && echo "$ ng build FINISHED" \
 && cp docs/index.html docs/404.html \
