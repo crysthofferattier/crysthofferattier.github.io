83f408ed3736a35357d5451766c00b4ebcec87c1
crysthofferattier <crysthofferattier@gmail.com>
2024-10-16 22:21:05 +0100

    Production version 2024-10-16 22:21:05

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# POOR App Version Management
+APP_VERSION_OLD=$(cat src/index.html | grep app-title | cut -d'.' -f 2)
+APP_VERSION=$(($APP_VERSION_OLD + 1))
+echo "App Version $APP_VERSION"
+
+sed -i "s/v0.$APP_VERSION_OLD/v0.$APP_VERSION/" src/index.html \
+&& ng build --verbose --output-path docs --base-href="https://crysthofferattier.github.io/" \
+&& echo "$ ng build FINISHED" \
+&& git add . && git commit -m "Production version `date +'%Y-%m-%d %H:%M:%S'`" && git push -u origin main
